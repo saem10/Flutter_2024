6b67b5ab9e214cdd41473ae03cae387f275722c4
saem10 <mirsaemhasan@gmail.com>
2024-12-02 19:22:27 +0600

    container

--- lib/main.dart
+++ lib/main.dart
@@ -50,42 +50,61 @@ class Home extends StatelessWidget {
           ],
           //leading: Icon(Icons.add),
         ),
-      body:
-      /*ListView.builder(
-        itemCount: friendlist.length,
-          itemBuilder: (BuildContext context, int index){
-        return Column(
-          children: [
-            Text(friendlist[index]),
-            Divider(
-              height: 12,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-            ),
+      body:Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
 
-          ],
-        );
-      }),*/
-      ListView.separated(
-          itemCount: friendlist.length,
-          itemBuilder:(BuildContext context, int index){
-            return Column(
-              children: [
-                Text(friendlist[index]),
-              ],
-            );
-          },
-          separatorBuilder: (BuildContext context, int index){
-            return Divider(
-              color: Colors.red,
-              indent: 10,
-              endIndent: 10,
-            );
-          })
+                margin: EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  border: Border.all(color: Colors.black,width: 2),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.pink.withOpacity(0.4),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0,2),
+                    ),
+                  ]
+                ),
+                child: Text('SaEm'),
+              ),
 
+              Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(8),
+                margin: EdgeInsets.all(8),
 
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  /*borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    bottomRight: Radius.circular(24)
+                  ),*/
+                  border:Border.all(color: Colors.red,width: 4),
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: NetworkImage('https://plus.unsplash.com/premium_photo-1672116453187-3aa64afe04ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'),
+                  fit: BoxFit.cover,
+                    opacity:0.5,
+                  ),
 
+                ),
+                child:
+                Text('SAEM'),
+              ),
+            ],
+          )
+        ],
+
+      )
     );
   }
 }
