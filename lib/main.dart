5ae586b4e050f91042ed09c70abaefa7859ab2d1
saem10 <mirsaemhasan@gmail.com>
2025-01-13 21:40:20 +0600

    Navigation Theme, Dark Theme, Light theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,95 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       title: 'Cheta',
       home: Home(),
+      theme: ThemeData(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ))),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.blueGrey, foregroundColor: Colors.white),
+          inputDecorationTheme: InputDecorationTheme(
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+          )
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ))),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.blueGrey, foregroundColor: Colors.white),
+          inputDecorationTheme: InputDecorationTheme(
+            border:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+            enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+            focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+          )
+      ),
+      themeMode: ThemeMode.dark,
     );
   }
 }
 
 class Home extends StatelessWidget {
   Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment:MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Profile();
-                  }));
-                },
-                child: Text('Go to Profile')),
-            Container(height: 10,),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Setting();
-                  }));
-                },
-                child: Text('Go to Setting'))
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(),
+              SizedBox(height: 12,),
+              TextField(),
+              SizedBox(height: 12,),
+              TextField(),
+              SizedBox(height: 12,),
+              Container(
+                height: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Profile();
+                    }));
+                  },
+                  child: Text('Go to Profile')),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Setting();
+                    }));
+                  },
+                  child: Text('Go to Setting'))
+            ],
+          ),
         ),
       ),
     );
@@ -51,6 +108,7 @@ class Home extends StatelessWidget {
 
 class Profile extends StatelessWidget {
   Profile({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,26 +118,28 @@ class Profile extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(onPressed: (){
-              Navigator.pop(context);
-            },
-                child:Text('Back'),),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Setting()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Setting()));
                 },
                 child: Text('Go to Setting')),
           ],
         ),
       ),
-
     );
   }
 }
 
 class Setting extends StatelessWidget {
   Setting({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,18 +149,49 @@ class Setting extends StatelessWidget {
       body: Center(
           child: Column(
         children: [
+
+          SizedBox(height: 16,),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.green,
+              ),
+              Positioned(
+                bottom: .5,
+                right: .5,
+                width: 50,
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  color: Color(0xFFF48FB1),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16,),
           ElevatedButton(
               onPressed: () {
                 Navigator.pushReplacement(context,
                     MaterialPageRoute(builder: (context) => Profile()));
               },
               child: Text('Go to Profile')),
-
-          ElevatedButton(onPressed: (){
-            Navigator.pushAndRemoveUntil(context,
-                MaterialPageRoute(builder: (context)=>Home()),
-                (predicate)=>false,);
-          },
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                  (predicate) => false,
+                );
+              },
               child: Text('go to home'))
         ],
       )),
