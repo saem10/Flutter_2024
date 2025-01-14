1686aea884f811243d14f64a809b5f2787509704
saem10 <mirsaemhasan@gmail.com>
2025-01-14 16:52:21 +0600

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-////Named Navigation, Statefull Widget
-
+//// Statefull Widget
 
 void main(){
   runApp(SimpleApp());
@@ -13,84 +12,36 @@ class SimpleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //home: Home(),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context)=> Home(),
-        '/profile': (context)=> Profile(),
-        '/setting': (context)=> Setting(),
+      home: CounterScreen(),
+
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child:Text('Center value is $counter') ,
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        counter= counter+1;
+        print(counter);
+        setState(() {});
       },
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Profile()));
-              },
-              child: Text('Go to Profile'))
-        ],
-      ),
-    );
-  }
-}
-
-class Profile extends StatelessWidget {
-  const Profile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/setting');
-            //Navigator.pushNamed(context, '/setting');
-          },
-              child: Text('Go to Setting'))
-        ],
-      ),
-    );
-  }
-}
-
-
-class Setting extends StatelessWidget {
-  const Setting({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Setting'),
-      ),
-
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: (){
-            //Navigator.push(context, MaterialPageRoute(builder: (context)=> Home()));
-            Navigator.pushReplacementNamed(context, '/home');
-          },
-              child: Text('go to Home'))
-        ],
-      ),
+      child: Icon(Icons.add),),
     );
   }
 }
